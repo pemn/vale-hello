739f2c737c824c59468b70925a63e9a424556339
Paulo Ernesto <pemn@users.noreply.github.com>
2018-10-02 08:07:29 -0300

    Add files via upload

---
+++ vale_hello.js
@@ -0,0 +1,16 @@
+//#!cscript
+// hello world script using the apache license
+/*
+Copyright 2018 Vale
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+*** You can contribute to the main repository at: ***
+
+https://github.com/pemn/vale-hello
+*/
+WScript.Echo("Hello World")
